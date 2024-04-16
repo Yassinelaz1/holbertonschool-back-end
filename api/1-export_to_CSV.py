48ed41308de30e88f03bfe0fad46360b10ea7006
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-04-17 00:40:42 +0100

    1-export_to_CSV.py

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+import sys
+import csv
+
+
+def to_do_list(employee_id):
+
+    todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    ).json()
+    response = requests.get(
+        f"https://jsonplaceholder.\
+typicode.com/users/{employee_id}"
+    ).json()
+    with open(f"{employee_id}.csv", mode="w") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow(
+                [(employee_id), response["username"],
+                 todo["completed"], todo["title"]]
+            )
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    to_do_list(employee_id)
