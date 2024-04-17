2037d73c253097b8ae3e2c6d61784a92335ad877
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-04-17 16:35:39 +0100

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-"""
-This script will use the REST API to return information
-about a given employee's TODO list progress.
-"""
+""" Python script to export data in the CSV format."""
 import csv
 import requests
 import sys
 
 
-def TODO_REQUESTS(ID):
-    """
-    extend your Python script to export data in the CSV format
-    """
-    todos = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={ID}"
-    ).json()
-    user_info = requests.get(
-        f"https://jsonplaceholder.\
-typicode.com/users/{ID}"
-    ).json()
-    with open(f"{ID}.csv", mode="w") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            writer.writerow(
-                [(ID), user_info["username"],
-                 todo["completed"], todo["title"]]
-            )
-
-
 if __name__ == "__main__":
-    ID = sys.argv[1]
-    TODO_REQUESTS(ID)
+    user_id = sys.argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
